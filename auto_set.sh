7717d71e6f55882c263d1638b094f45377e41dac
Edoardo Fabrizio De Iovanna <edoardofabriziodeiovanna@hotmail.com>
2024-10-16 13:00:22 -0300

    add set grub default only find windows boot manager

--- auto_set.sh
+++ auto_set.sh
@@ -4,7 +4,7 @@
 KERNEL_VERSION=$(uname -r)
 
 # Read the kernel version from the file.
-FILE_KERNEL_VERSION=$(cat kernel_version.txt)
+FILE_KERNEL_VERSION=$(cat "$HOME/kernel_version.txt")
 
 # Check if the file exists and is not empty.
 if [[ -n "$FILE_KERNEL_VERSION" ]]; then
@@ -15,9 +15,12 @@ if [[ -n "$FILE_KERNEL_VERSION" ]]; then
         WINDOWS_DEVICE=$(sudo awk -F\' '$1=="menuentry " {print $2}' /boot/grub2/grub.cfg)
         echo "$WINDOWS_DEVICE"
 
-        # Set the default boot entry to the Windows device.
-        echo $(sudo grub2-set-default "$WINDOWS_DEVICE")
-        echo $(sudo grub2-mkconfig -o /boot/grub2/grub.cfg)
+        # Check if the $WINDOWS_DEVICE contain the "Windows Boot Manager" substring.
+        if [[ "$WINDOWS_DEVICE" == *"Windows Boot Manager"* ]]; then
+            # Set the default boot entry to the Windows device.
+            echo $(sudo grub2-set-default "$WINDOWS_DEVICE")
+            echo $(sudo grub2-mkconfig -o /boot/grub2/grub.cfg)
+        fi
     else
         # If the kernel version has not changed, print a message.
         echo "No changes are necessary to the grub file."
@@ -26,7 +29,7 @@ fi
 
 # Create or edit kernel.txt to check the kernel version for later setting of the default GRUB entry.
 echo "create or edit 'kernel_version.txt' file..."
-printf "%s\n" $KERNEL_VERSION > kernel_version.txt
+printf "%s\n" $KERNEL_VERSION > "$HOME/kernel_version.txt"
 
 # End script.
 echo "done script."
