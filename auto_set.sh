23f29a2a5014698773d4074a74f2e8df087f8a10
Edoardo Fabrizio De Iovanna <edoardofabriziodeiovanna@hotmail.com>
2024-12-12 08:46:48 -0300

    refactor script

--- auto_set.sh
+++ auto_set.sh
@@ -1,38 +1,34 @@
 #!/bin/bash
 
-# Get the current kernel version.
-KERNEL_VERSION=$(uname -r)
+# Get the current default GRUB entry
+DEFAULT_ENTRY=$(sudo grub2-editenv list | awk -F= '/saved_entry/ {print $2}')
+echo "Default Entry: $DEFAULT_ENTRY"
 
-# Read the kernel version from the file.
-FILE_KERNEL_VERSION=$(cat "$HOME/kernel_version.txt")
+# Extract the Windows device entry from the GRUB configuration file
+WINDOWS_DEVICE=$(sudo awk -F\' '$1=="menuentry " && /Windows Boot Manager/ {print $2}' /boot/grub2/grub.cfg)
+echo "Windows Entry: $WINDOWS_DEVICE"
 
-# Check if the file exists and is not empty.
-if [[ -n "$FILE_KERNEL_VERSION" ]]; then
-
-    # Check if the kernel version has changed / updated.
-    if [[ "$KERNEL_VERSION" != "$FILE_KERNEL_VERSION" ]]; then
-        # Extract the Windows device path from the GRUB configuration file.
-        WINDOWS_DEVICE=$(sudo awk -F\' '$1=="menuentry " {print $2}' /boot/grub2/grub.cfg)
-        echo "$WINDOWS_DEVICE"
-
-        # Check if the $WINDOWS_DEVICE contain the "Windows Boot Manager" substring.
-        if [[ "$WINDOWS_DEVICE" == *"Windows Boot Manager"* ]]; then
-            # Set the default boot entry to the Windows device.
-            echo $(sudo grub2-set-default "$WINDOWS_DEVICE")
-            echo $(sudo grub2-mkconfig -o /boot/grub2/grub.cfg)
-        else
-            echo "Not found not any Windows Devices."
-        fi
-    else
-        # If the kernel version has not changed, print a message.
-        echo "No changes are necessary to the grub file."
-    fi
+# Exit if Windows Boot Manager is not found
+if [[ -z "$WINDOWS_DEVICE" ]]; then
+    echo "Windows Boot Manager not found in the GRUB configuration file."
+    echo "Please ensure Windows is installed and properly detected by GRUB."
+    echo "You may try regenerating the GRUB configuration file with:"
+    echo "sudo grub2-mkconfig -o /boot/grub2/grub.cfg"
+    exit 1
 fi
 
-# Create or edit kernel.txt to check the kernel version for later setting of the default GRUB entry.
-echo "create or edit 'kernel_version.txt' file located at HOME/kernel_version.txt..."
-printf "%s\n" $KERNEL_VERSION > "$HOME/kernel_version.txt"
+# Exit if Windows is already the default
+if [[ "$DEFAULT_ENTRY" == "$WINDOWS_DEVICE" ]]; then
+    echo "Windows is already set as the default boot option in GRUB."
+    echo "Script completed."
+    exit 0
+fi
 
-# End script.
-echo "done script."
+# Set Windows as the default boot option
+echo "Setting Windows as the default boot option in GRUB..."
+sudo grub2-set-default "$WINDOWS_DEVICE"
+sudo grub2-mkconfig -o /boot/grub2/grub.cfg
+
+# End of script
+echo "Script completed."
 exit 0
