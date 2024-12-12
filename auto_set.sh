3cf78544ad3d67223c97f988b36855455d71850e
Edoardo Fabrizio De Iovanna <edoardofabriziodeiovanna@hotmail.com>
2024-12-12 10:19:08 -0300

    refactoring code

--- auto_set.sh
+++ auto_set.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
 
-# Get the current default GRUB entry
-DEFAULT_ENTRY=$(sudo grub2-editenv list | awk -F= '/saved_entry/ {print $2}')
-echo "Default Entry: $DEFAULT_ENTRY"
-
 # Extract the Windows device entry from the GRUB configuration file
-WINDOWS_DEVICE=$(sudo awk -F\' '$1=="menuentry " && /Windows Boot Manager/ {print $2}' /boot/grub2/grub.cfg)
-echo "Windows Entry: $WINDOWS_DEVICE"
+WINDOWS_ENTRY=$(sudo awk -F\' '$1=="menuentry " && /Windows Boot Manager/ {print $2}' /boot/grub2/grub.cfg)
+echo "Windows Entry: $WINDOWS_ENTRY"
 
 # Exit if Windows Boot Manager is not found
-if [[ -z "$WINDOWS_DEVICE" ]]; then
+if [[ -z "$WINDOWS_ENTRY" ]]; then
     echo "Windows Boot Manager not found in the GRUB configuration file."
     echo "Please ensure Windows is installed and properly detected by GRUB."
     echo "You may try regenerating the GRUB configuration file with:"
@@ -17,8 +13,12 @@ if [[ -z "$WINDOWS_DEVICE" ]]; then
     exit 1
 fi
 
+# Get the current default GRUB entry
+DEFAULT_ENTRY=$(sudo grub2-editenv list | awk -F= '/saved_entry/ {print $2}')
+echo "Default Entry: $DEFAULT_ENTRY"
+
 # Exit if Windows is already the default
-if [[ "$DEFAULT_ENTRY" == "$WINDOWS_DEVICE" ]]; then
+if [[ "$DEFAULT_ENTRY" == "$WINDOWS_ENTRY" ]]; then
     echo "Windows is already set as the default boot option in GRUB."
     echo "Script completed."
     exit 0
@@ -26,7 +26,7 @@ fi
 
 # Set Windows as the default boot option
 echo "Setting Windows as the default boot option in GRUB..."
-echo $(sudo grub2-set-default "$WINDOWS_DEVICE")
+echo $(sudo grub2-set-default "$WINDOWS_ENTRY")
 echo $(sudo grub2-mkconfig -o /boot/grub2/grub.cfg)
 
 # End of script
